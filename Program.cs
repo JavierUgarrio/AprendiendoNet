168281c140dce012b639dc76fed6dcee4dab6132
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-04-16 10:56:28 +0200

    variables ejemplos

--- Program.cs
+++ Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string mensaje = Console.ReadLine();
+            int edad= 15;
+            bool verdadero;
+            int? altura; // con el ? puedes decir que la varible puede ser nula
+
+            int valorDefecto = default(int); // valor 0
+            bool valorDefectoBooleano = default(bool); // valor FALSE
+
+            edad = edad + 5;
+
+            if(edad == null)
+            {
+                //hacer algo
+            }
+
+            DateTime? fecha = new DateTime();
+
+            int? dia = fecha?.Day;
+
+            Console.WriteLine(dia);
+
+            Console.WriteLine(edad);
+
+            //inferir en una variable
+
+            var variable = "Hola";
+
+            var variable2 = edad + "hola interfiero";
+
         }
     }
 }
--- obj/Debug/net6.0/IntroduccionNet.AssemblyInfo.cs
+++ obj/Debug/net6.0/IntroduccionNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntroduccionNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f803d6b6444e0591ce4c270941b2fd865aef0ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntroduccionNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntroduccionNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
